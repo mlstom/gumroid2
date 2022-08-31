751ec691ba4af5f9fe5d933c182a00b21a247beb
Milos Tomic <milostomic502@gmail.com>
2022-08-31 10:33:00 +0200

    dodati podaci

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import {Routes,Route,useNavigate} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 function App() {
   return (
     <div className="App">
-      Main
+      <Routes>
+        <Route path="login" element={<Login/>} />
+        <Route path="/*" element={<Home/>} />
+    </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Hero from '../sections/Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
